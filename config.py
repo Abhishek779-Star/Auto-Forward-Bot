85f92ab8b3ad90348799534a9b827ce5d8214762
Abhishek779-Star <abhishekbro779@gmail.com>
2026-01-27 20:01:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "16402669"))
+API_HASH = os.environ.get("API_HASH", "e50b6c6e9dc8077ec3e9db0e565631e4")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-MONGO_URI = os.environ.get("MONGO_URI", "")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://Abhishek:abhishek@abhishek.3er4sy0.mongodb.net/?appName=abhishek")
 DB_NAME = os.environ.get("DB_NAME", "SilentXBotz")
 
 WEB_SERVER = os.environ.get("WEB_SERVER", "True").lower() in ("true", "1", "t")
